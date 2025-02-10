08cdcd3ab1bbb53a795f5509509e173264e716e2
cendyz <arek57823@gmail.com>
2025-02-10 12:26:07 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,14 @@
 		<img :src="mobileBg" alt="" class="absolute w-full h-full top-0 left-0" />
 	</picture>
 	<main class="relative w-[87vw]">
+<<<<<<< HEAD
 		<section class="bg-blue-0 p-[4rem] w-full grid gap-y-[3.5rem] rounded-2xl rounded-tr-[10rem]">
 			<img :src="logo" alt="three squares on top of each other and
 			text fylo" />
+=======
+		<section class="bg-blue-0 p-[4rem] grid gap-y-[3.5rem] rounded-2xl rounded-tr-[10rem]">
+			<img :src="logo" alt="three squares on top of each other and text fylo" />
+>>>>>>> d514de4 (test)
 			<div class="flex gap-x-[1.5rem] items-center">
 				<div
 					v-for="item in data"
@@ -16,6 +21,7 @@
 				</div>
 			</div>
 		</section>
+<<<<<<< HEAD
     <section class="bg-blue-0 p-[4rem] w-full grid gap-y-[1.5rem] rounded-2xl mt-[2rem]">
       <p class="text-blue-50 opacity-70 text-[1.5rem] text-center">You've used <span class="font-bold uppercase">815 gb</span> of your storage</p>
       <input type="range">
@@ -25,6 +31,8 @@
       </div>
       <div class="flex items-center text-[4.5rem] font-bold bg-white w-fit py-[.5rem] px-[2rem]">185 <span class="uppercase text-[1.4rem]">gb left</span></div>
     </section>
+=======
+>>>>>>> d514de4 (test)
 	</main>
 </template>
 
